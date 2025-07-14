4e24c648374310f69fe2e8141817d4ceea72684a
praveenreddie31 <praveenreddie31@gmail.com>
2025-07-15 00:00:27 +0530

    all codes

--- app.py
+++ app.py
@@ -370,7 +370,7 @@ def main():
         
         if uploaded_file:
             image = Image.open(uploaded_file)
-            st.image(image, caption="📱 Uploaded Image", use_column_width=True)
+            st.image(image, caption="📱 Uploaded Image", use_container_width=True)
     
     with col2:
         if uploaded_file:
